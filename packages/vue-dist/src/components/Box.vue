b8ac8694199088fd70c4f7d9a6c87e08d2bd9513
haoming <435203093@qq.com>
2021-03-17 01:13:00 +0800

    Update Box.vue

--- packages/vue-dist/src/components/Box.vue
+++ packages/vue-dist/src/components/Box.vue
@@ -1,5 +1,5 @@
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent,ref } from "vue";
 import { Button, Modal, AutoComplete, message } from "ant-design-vue";
 import { SettingOutlined } from "@ant-design/icons-vue";
 import Cookie from "js-cookie";
@@ -7,8 +7,8 @@ import Cookie from "js-cookie";
 export default defineComponent({
   setup() {
     const visible = ref(false);
-    const domain = ref(Cookie.get("_domain"));
 
+    const domain = ref(Cookie.get("_domain"));
     const options = ref(
       window._domains ||
         [
@@ -44,7 +44,8 @@ export default defineComponent({
               left: "0px",
               bottom: "5px",
               opacity:'0.7',
-              padding:'0px'
+              padding:'0px',
+              zIndex:100000
             }}
             onClick={() => (visible.value = true)}
           >
